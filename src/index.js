416ce169d389d48f14f29fe5c2ed2e234d4c8406
francine babauta <francine.babauta@outlook.com>
2021-03-04 09:44:18 -0800

    service worker added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
+import * as serviceWorker from './serviceWorker';
 
 
 ReactDom.render(<App />, document.getElementById("root"));
