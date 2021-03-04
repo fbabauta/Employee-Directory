c31b5411ced3a55fe966ff1af4437c32c4c09f02
francine babauta <francine.babauta@outlook.com>
2021-03-04 09:50:44 -0800

    file restructuring

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,10 @@
-/* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
 
+const URL = "https://randomuser.me/api/?results=50&nat=us"
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    getEmployees: function () {
-        return axios.get("https://randomuser.me/api/?results=200&nat=u");
+    getEmployees: function() {
+        return axios.get(URL);
     }
-};
+}
